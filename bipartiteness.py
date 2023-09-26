eb439077cf03c7f412feb5ecc9692698aff2f1e6
thomasthunes <100432291+thomasthunes@users.noreply.github.com>
2023-09-26 18:12:21 +0200

    Update bipartiteness.py

--- bipartiteness.py
+++ bipartiteness.py
@@ -1,41 +1,51 @@
 from collections import namedtuple as T
 from collections import deque
 
-Graph = T("Graph", "V N")
 
-G = Graph(V=[0, 1, 2, 3], N=[[1, 2], [0, 2, 3], [0, 1], [1]])
-G1 = Graph(V=[0, 1, 2, 3], N=[[1, 3], [0], [3], [2, 0]])
-
-
-# for v in G1.V:
-#    print(v, G1.N[v])
+def isBipartite(p):
+    for elem in p[0]:
+        if elem in p[1]:
+            return False
+    return True
 
 
-def f(G): # total runtime is O(V + E)
-    queue = deque([G.V[0]])   # O(1)
-    visited = []   # O(1)
-    label_partition = {  # O(1)
+def bipartiteLabelPartition(G):
+    queue = deque([G.V[0]])
+    visited = []
+    label_partition = {
         0: [G.V[0]],
         1: []
     }
-
     label = 1
-    while len(queue) != 0:  # O(nodes)
-        node = queue.popleft()  # O(1)
-        visited.append(node)   # O(1)
+    while len(queue) != 0:
+        node = queue.popleft()
+        visited.append(node)
 
-        all_neigh_unvisited = True  # O(1)
-        for neigh in G.N[node]:  # O(edges)
-            if neigh not in visited:  # O(1)
-                queue.append(neigh)  # O(1)
-                label_partition[label].append(neigh)  # O(1)
+        all_neigh_unvisited = True
+        for neigh in G.N[node]:
+            if neigh not in visited:
+                queue.append(neigh)
+                label_partition[label].append(neigh)
             else:
-                all_neigh_unvisited = False   # O(1)
+                all_neigh_unvisited = False
 
-        if all_neigh_unvisited:  # O(1)
-            label = abs(label - 1)  # O(1)
+        if all_neigh_unvisited:
+            label = abs(label - 1)
+
+    if isBipartite(label_partition):
+        print("The graph is a bipartite graph")
+        print(label_partition)
+        return label_partition
+    print("The graph is an odd cycle")
+    print(label_partition)
     return label_partition
 
 
-print(f(G1))
-print(f(G))
+Graph = T("Graph", "V N")
+
+G0 = Graph(V=[0, 1, 2, 3], N=[[1, 2], [0, 2, 3], [0, 1], [1]])
+G1 = Graph(V=[0, 1, 2, 3], N=[[1, 3], [0], [3], [2, 0]])
+
+bipartiteLabelPartition(G0)
+bipartiteLabelPartition(G1)
+
